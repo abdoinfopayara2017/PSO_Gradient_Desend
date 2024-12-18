e43cb38bfd36c6482528aa91d27851e7abf66a3d
abdoinfopayara2017 <ali19202010@gmail.com>
2024-12-18 10:46:36 +0100

    high dice

--- LUNA16Challege/Vnet/Implementation/PSOimplementation.py
+++ LUNA16Challege/Vnet/Implementation/PSOimplementation.py
@@ -27,41 +27,51 @@ class PSOimplemntation :
             variables = pickle.load(file)
         return variables
      
-    def lunch(self):     
+    def lunch(self,retrive):     
 
      PSO=PSOEngine(self.swarm_size,self.cognitive,self.social,self.weight,0)
-     list_particules=[]    
-     list_particules=PSO.init_particles(list_particules)
-     gbest=np.empty(len(list_particules[0].position),dtype=object)
-     for w in range(0,len(gbest)):            
-        gbest[w]=tf.Variable(initial_value=tf.constant(0.0,shape=list_particules[0].position[w].get_shape()),
-                    shape=list_particules[0].position[w].get_shape(),dtype=tf.float32)      
-     gbest_fitness=tf.Variable(0,dtype=tf.float32)
+     list_particules=[]
+     path = "log\\segmeation\\" + "model\\" 
+     if not os.path.exists(path) :
+        os.makedirs(path)
      
-     # initialisation des parametres 
-     for p in range(0,len(list_particules)) :        
-      
-      list_particules[p].fitness , list_particules[p].partial_derivative ,PSO.index_in_epoch = \
-        PSO.evaluate_fitness(list_particules[p].position)
+     if retrive :
+        list_particules=self.retrieveVariables(path+"model.txt")
+        gbest=np.empty(len(list_particules[0].position),dtype=object)
+        for w in range(0,len(gbest)):            
+          gbest[w]=tf.Variable(initial_value=tf.constant(0.0,shape=list_particules[0].position[w].get_shape()),
+                    shape=list_particules[0].position[w].get_shape(),dtype=tf.float32)
+        gbest_fitness=tf.Variable(0,dtype=tf.float32)
+     else :
+        list_particules=[]    
+        list_particules=PSO.init_particles(list_particules)
+        gbest=np.empty(len(list_particules[0].position),dtype=object)
+        for w in range(0,len(gbest)):            
+          gbest[w]=tf.Variable(initial_value=tf.constant(0.0,shape=list_particules[0].position[w].get_shape()),
+                    shape=list_particules[0].position[w].get_shape(),dtype=tf.float32)      
+        gbest_fitness=tf.Variable(0,dtype=tf.float32)
+     
+        # initialisation des parametres 
+        for p in range(0,len(list_particules)) :      
+          list_particules[p].fitness , list_particules[p].partial_derivative ,PSO.index_in_epoch = \
+           PSO.evaluate_fitness(list_particules[p].position)
         
-      with tf.device('/gpu:0'):
-        for w in range(0,len(list_particules[p].partial_derivative)) :         
-          list_particules[p].partial_derivative[w]=tf.where(
+          with tf.device('/gpu:0'):
+            for w in range(0,len(list_particules[p].partial_derivative)) :         
+              list_particules[p].partial_derivative[w]=tf.where(
               tf.greater_equal(list_particules[p].partial_derivative[w],tf.constant(0,dtype=tf.float32))\
-            ,tf.ones_like(list_particules[p].partial_derivative[w]),- tf.ones_like(list_particules[p].partial_derivative[w]))
+              ,tf.ones_like(list_particules[p].partial_derivative[w]),- tf.ones_like(list_particules[p].partial_derivative[w]))
                 
-      # for each particle i do Pbesti = xi;
-      list_particules[p].fitness_best_pos.assign( list_particules[p].fitness)     
-      for w in range(0,len(list_particules[p].position)):
-       list_particules[p].best_pos[w].assign (list_particules[p].position[w])
+          # for each particle i do Pbesti = xi;
+          list_particules[p].fitness_best_pos.assign( list_particules[p].fitness)     
+          for w in range(0,len(list_particules[p].position)):
+           list_particules[p].best_pos[w].assign (list_particules[p].position[w])
           
      # Find best particle in set
      gbest , gbest_fitness=PSO.find_gbest(list_particules,gbest,gbest_fitness)
      
      last_fitness = gbest_fitness.numpy()
-     path = "log\\segmeation\\" + "model\\" 
-     if not os.path.exists(path) :
-        os.makedirs(path)       
+            
      # PSO boucle
      # for each iteration do
      with tf.device('/gpu:0'):
@@ -102,7 +112,7 @@ class PSOimplemntation :
                         list_particules[j].fitness_best_pos).numpy()
             if (bool):
              list_particules[j].fitness_best_pos =  list_particules[j].fitness
-             for w in range(0,len(list_particules[p].position)):
+             for w in range(0,len(list_particules[j].position)):
               list_particules[j].best_pos[w].assign (list_particules[j].position[w])
                      
           #update Gbest 
@@ -126,13 +136,13 @@ class PSOimplemntation :
         #      % (gbest_fitness.numpy()))                    
        
 
-def launch_pso():
+def launch_pso(retrive):
    
-     psoimplemntation = PSOimplemntation(nb_iteration=2451,
+     psoimplemntation = PSOimplemntation(nb_iteration=2717,
                           swarm_size=20,cognitive=0.000018,social=0.000002,weight=0.9)
-     psoimplemntation.lunch()
+     psoimplemntation.lunch(retrive)
 
-launch_pso()    
+launch_pso(True)    
 
 
            
