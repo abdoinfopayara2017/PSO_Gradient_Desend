b6845adf4bf506d2cd15f5274e89a718f5ff50a5
felioune abderrezzak <felioune.abderrezzak@airalgerie.dz>
2025-04-11 11:44:41 +0100

    test with adam

--- LUNA16Challege/Vnet/Implementation/PSOimplementation.py
+++ LUNA16Challege/Vnet/Implementation/PSOimplementation.py
@@ -241,8 +241,8 @@ def launch_pso(retrive):
      psoimplemntation = PSOimplemntation(nb_iteration=5435,
                           swarm_size=20,cognitive=0.00018,social=0.002,weight=0.9)
      psoimplemntation.lunch(retrive,imagedata,maskdata,3)
-#launch_pso(False)      
-predict_test()      
+launch_pso(False)      
+#predict_test()      
 
 
            
